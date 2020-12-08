f414fee627931858ea08212e1068026d386f4dfe
Johannes Neumeier <hello@johannesneumeier.com>
2020-12-08 13:44:48 +0200

    Added ttfautohinting

--- tools/build.sh
+++ tools/build.sh
@@ -16,13 +16,13 @@ fontmake -g "sources/Rasa Italics-MM.glyphs" -o ufo --interpolate --master-dir=$
 # Loop through the instances and prepare and compile each of them
 for file in $(ls $INSTANCES); do
 
-    echo "Preparing $INSTANCES/$file"
     # Make sure mark features get written without any "design" anchors left over
+    echo "Preparing $INSTANCES/$file"
     python tools/remove-anchors-from-ufo.py $INSTANCES/$file periodcentred _periodcentred
 
 
-    echo "Write upright features to $INSTANCES/$file"
     # Combine and write our custom features to the UFOs
+    echo "Write upright features to $INSTANCES/$file"
     if [[ "$file" == *Italic.ufo* ]]; then
         python tools/parse-features.py production/features/italics.fea $INSTANCES/$file
     else
@@ -31,8 +31,13 @@ for file in $(ls $INSTANCES); do
 
 
     echo "Compiling $FONTS/${file/ufo/ttf}"
-    # Compile OTF fonts
     fontmake -u $INSTANCES/$file --output ttf --output-dir $FONTS --debug-feature-file debug.fea
+
+
+    echo "Autohinting $FONTS/${file/ufo/ttf}"
+    ttfautohint $FONTS/${file/ufo/ttf} $FONTS/${file/ufo/ttf}-hinted
+    cp $FONTS/${file/ufo/ttf}-hinted $FONTS/${file/ufo/ttf}
+    rm $FONTS/${file/ufo/ttf}-hinted
 
     gftools fix-dsig --autofix $FONTS/${file/ufo/ttf}
 done
