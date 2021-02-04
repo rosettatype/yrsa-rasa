2568efddf1577fb8932d2af6b9a3928e5ac90b00
Yanone <post@yanone.de>
2021-02-04 13:33:57 +0100

    Tweaked build.sh (mostly copy .ttf.fix back to .ttf)

--- tools/build.sh
+++ tools/build.sh
@@ -282,9 +282,11 @@ if [ "$VF" == 1 ] && ([ "$TTF" == 1 ] || [ "$OTF" == 1 ]); then
             fontmake -m $DS -o variable --output-path=$FILE --flatten-components
 
             # Add STAT table
+            echo "Add STAT table"
             statmake --designspace $DS --stylespace $SS $FILE
 
-            gftools fix-nonhinting $FILE
+            gftools fix-nonhinting $FILE $FILE.fix
+            mv $FILE.fix $FILE
             rm -f "${FILE/.ttf/-backup-fonttools-prep-gasp.ttf}"
 
             gftools fix-unwanted-tables $FILE
@@ -335,12 +337,13 @@ if [ "$VF" == 1 ] && ([ "$TTF" == 1 ] || [ "$OTF" == 1 ]); then
 
             python tools/replace-family-name.py $FILE Rasa Yrsa
 
-            gftools fix-nonhinting $FILE
+            gftools fix-nonhinting $FILE $FILE.fix
+            mv $FILE.fix $FILE
             rm -f "${FILE/.ttf/-backup-fonttools-prep-gasp.ttf}"
             
             gftools fix-unwanted-tables $FILE
 
-            gftools fix-dsig --autofix $file
+            gftools fix-dsig --autofix $FILE
         done
     fi
 fi
