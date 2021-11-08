cdafcc70da471f0b8ba847476c2a2113b716d66a
Johannes Neumeier <hello@johannesneumeier.com>
2021-11-08 12:16:50 +0100

    Added script for easier release creation

---
+++ tools/release.sh
@@ -0,0 +1,48 @@
+
+echo "Release archive helper script, for maintainers of the repo :)"
+
+if [$1 == ""]
+then
+    echo "Pass in a new version tag to create, increment at least from below and match what is in the font files!"
+    git tag
+    exit
+fi
+
+
+echo "Tagging version $1"
+git tag -f $1
+
+
+RASA="release/Rasa-fonts-$1"
+echo "Packaging $RASA.zip"
+mkdir $RASA
+cp documentation/FONTLOG.md $RASA/FONTLOG.txt
+cp OFL.txt $RASA/OFL.txt
+cp README.MD $RASA/README.txt
+mkdir $RASA/otf
+mkdir $RASA/ttf
+mkdir $RASA/variable
+cp fonts/Rasa/*.otf $RASA/otf/
+cp fonts/Rasa/*.ttf $RASA/ttf/
+cp fonts/RasaVariable/* $RASA/variable/
+zip $RASA.zip $RASA
+rm -r $RASA
+
+
+YRSA="release/Yrsa-fonts-$1"
+echo "Packaging $YRSA.zip"
+mkdir $YRSA
+cp documentation/FONTLOG.md $YRSA/FONTLOG.txt
+cp OFL.txt $YRSA/OFL.txt
+cp README.MD $YRSA/README.txt
+mkdir $YRSA/otf
+mkdir $YRSA/ttf
+mkdir $YRSA/variable
+cp fonts/Yrsa/*.otf $YRSA/otf/
+cp fonts/Yrsa/*.ttf $YRSA/ttf/
+cp fonts/YrsaVariable/* $YRSA/variable/
+zip $YRSA.zip $YRSA
+rm -r $YRSA
+
+
+echo "Push the tag (and commit) to github with '$ git push origin master --tags' and attach the generated zip files in /release to the new release :)"
